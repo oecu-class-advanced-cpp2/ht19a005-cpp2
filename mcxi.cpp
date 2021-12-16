e5a0523edf1dc98a4bfa756b506c3ee8d9379921
ht19a005 <91307801+ht19a005@users.noreply.github.com>
2021-12-16 18:57:31 +0900

    Create mcxi.cpp

---
+++ mcxi.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+namespace cpp2 {
+ /* --------------------------------------------------------------------- */
+ /*
+ mcxi
+ mcxi 記法を解析するクラスです。
+ */
+ /* --------------------------------------------------------------------- */
+ class mcxi {
+
+ };
+} // namespace cpp2
+int main(){
+ cpp2::mcxi a0("xi");
+ cpp2::mcxi b0("x9i");
+ cpp2::mcxi result0 = a0 + b0;
+ std::cout << "3x" <<" " << result0.to_string() << std::endl;
+ cpp2::mcxi a1("i");
+ cpp2::mcxi b1("9i");
+ cpp2::mcxi result1 = a1 + b1;
+ std::cout << "x" << " " << result1.to_string() << std::endl;
+ cpp2::mcxi a2("c2x2i");
+ cpp2::mcxi b2("4c8x8i");
+ cpp2::mcxi result2 = a2 + b2;
+ std::cout << "6cx" << " " << result2.to_string() << std::endl;
